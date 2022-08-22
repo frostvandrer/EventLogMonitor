feed9cf454f70ad3ce766d61cdb5599deefa1f32
UjoQurius <patrik.pis@gmail.com>
2022-08-22 21:01:12 +0200

    Powershell EventID 4104 parsing

---
+++ EventLogParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Text;
+
+namespace EventLogMonitor
+{
+    internal class EventLogParser
+    {
+        private EventLogReader Reader { get; set; }
+
+        public EventLogParser(string logName)
+        {
+            EventLogQuery query = new EventLogQuery(logName, PathType.LogName);
+            query.ReverseDirection = true;
+
+            Reader = new EventLogReader(query);
+        }
+           
+        public string ReadLog()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            using(Reader)
+            {
+                for (EventRecord rec = Reader.ReadEvent(); rec != null; rec = Reader.ReadEvent())
+                {
+                    bool happenedToday = rec.TimeCreated > DateTime.Now.AddDays(-1);
+
+                    if (rec.Id == 4104 && happenedToday)
+                    {
+                        sb.Append($"Event ID: {rec.Id}\n");
+                        sb.Append($"Event Name: {rec.TaskDisplayName}\n");
+                        sb.Append($"Timestamp: {rec.TimeCreated}\n\n");
+                    }
+                }
+            }
+                    
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EventLogMonitor
+{
+    internal class Tray : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        ContextMenuStrip menu;
+
+        public Tray()
+        {
+            trayIcon = new NotifyIcon();
+            menu = new ContextMenuStrip();
+
+            trayIcon.Icon = new System.Drawing.Icon("C:\\Users\\patri\\Documents\\Obsidian\\htb.ico");
+            trayIcon.ContextMenuStrip = menu;
+
+            ToolStripMenuItem readLog = new ToolStripMenuItem("Read log");
+            readLog.Click += new EventHandler(ShowLastLog);
+
+            ToolStripMenuItem exitMenu = new ToolStripMenuItem("Exit");
+            exitMenu.Click += new EventHandler(Exit);
+
+            menu.Items.Add(readLog);
+            menu.Items.Add(exitMenu);
+
+            trayIcon.Visible = true;
+        }
+
+        void ShowLastLog(object sender, EventArgs e)
+        {
+            EventLogParser parser = new EventLogParser("Microsoft-Windows-PowerShell/Operational");
+            MessageBox.Show(parser.ReadLog());
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
